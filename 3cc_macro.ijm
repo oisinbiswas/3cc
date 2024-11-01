2904137011c5e5ad5b6e0f45c0a0bc7f71e4e5b4
oisinbiswas <oisin-biswas@idexx.com>
2024-11-01 10:12:44 -0400

    Make file paths generic, allow for user to enter custom path for data analysis and output

--- 3cc_macro.ijm
+++ 3cc_macro.ijm
@@ -1,5 +1,5 @@
-input = "C:/Users/obiswas/OneDrive - IDEXX/OneDrive/3qc/transition_testing/";
-output = "C:/Users/obiswas/OneDrive - IDEXX/OneDrive/3qc/transition_output/";
+input = "enter/path/to/test_images/";
+output = "enter/path/to/macro_output/";
 run("Plots...", "width=1000 height=340 font=14 draw_ticks list minimum=0 maximum=0 interpolate");
 
 function action(input, output, filename) {
@@ -7,7 +7,7 @@ function action(input, output, filename) {
         makeLine(234,396,1612,416,1610,540,244,510,238,640,1606,636,1590,754,238,740,240,874,1586,868);
         run("Plot Profile");
         selectWindow("Plot Values");
-		saveAs("Results", "C:/Users/obiswas/OneDrive - IDEXX/OneDrive/3qc/transition_output/" + filename + ".csv" );
+		saveAs("Results", "enter/path/to/macro_output/" + filename + ".csv" );
 }
 
 
--- data_analysis.py
+++ data_analysis.py
@@ -8,8 +8,8 @@ import os
 
 
 
-input = r'C:\Users\obiswas\OneDrive - IDEXX\OneDrive\3qc\transition_output'
-output = r'C:\Users\obiswas\OneDrive - IDEXX\OneDrive\3qc\transition_csv'
+input = r'enter\path\to\macro_output'
+output = r'enter\path\to\\py_output'
 
 for root, dirs, files in os.walk(input, topdown=False):
     for i in files:
@@ -21,15 +21,9 @@ for root, dirs, files in os.walk(input, topdown=False):
         plt.ylim([0,100])
         plt.plot(peaks, x[peaks], "x")
         # plt.show()
-        # print(peaks.size, i)
-        # print(df, i)
-        #TODO - create dataframe with 2 columns, file name and num_peaks, export to csv after analysis 
         data = {peaks.size}
         df2 = pd.DataFrame(data, columns=["Number of Peaks"])
         print(df2)
-        # df2 = peaks.size
-        # print(df, i)
-        df.to_csv(output + "\\" + i + ".csv")
-        df2.to_csv(output + "\\" + i + "num_peaks" + ".csv")
-        #TODO if the average intensity is above or below, if the number of peaks is above or below, then classify 
+        df.to_csv(output + "\\" + i + "_light_intensity_data" + ".csv")
+        df2.to_csv(output + "\\" + i + "_num_peaks" + ".csv")
         
